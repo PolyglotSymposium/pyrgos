d8bd575de300dc9f3ffcd520ade3053efb549b46
Kazark <kazark@zoho.com>
2019-04-30 22:43:13 -0400

    Don't allow type shadowing

--- types.ml
+++ types.ml
@@ -18,16 +18,28 @@ let isDataCtr (g : gamma) (v : symbol) : texpr option =
      | [d] -> Some (TVar d.name)
      | _ -> None
 
-exception DataCtrAlreadyDefinedWithSameName of symbol*texpr*texpr
+exception DataCtrAlreadyDefined of symbol*texpr*texpr
 
-let registerDataType (d : data) (g : gamma) : gamma =
+let disallowShadowingCtrs (d : data) (g : gamma) : unit =
   List.iter (fun ctr ->
     match isDataCtr g ctr with
-    | None -> () (* Good. Not going to allow shadowing for now. *)
+    | None -> ()
     | Some existing ->
       let newT = TVar d.name in
-      raise (DataCtrAlreadyDefinedWithSameName (ctr, newT, existing))
-  ) d.ctrs;
+      raise (DataCtrAlreadyDefined (ctr, newT, existing))
+  ) d.ctrs
+
+exception TypeAlreadyDefined of symbol
+
+let disallowShadowingTypes (d : data) (g : gamma) : unit =
+  let xs = List.find_all (fun x -> x.name = d.name) g.datas
+  in match xs with
+     | [] -> ()
+     | _ -> raise (TypeAlreadyDefined d.name)
+
+let registerDataType (d : data) (g : gamma) : gamma =
+  disallowShadowingCtrs d g;
+  disallowShadowingTypes d g;
   (* TODO what safety checks are needed here? *)
   { g with datas = d :: g.datas }
 
