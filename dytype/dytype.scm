689c749bc95a36fbb97982630c0a38358f2ed466
Michael Gilliland <mjg.py3@gmail.com>
2019-07-20 12:58:31 -0400

    Throw some comments into the `if` synthesize code per #1 discussion.

--- dytype/dytype.scm
+++ dytype/dytype.scm
@@ -94,8 +94,19 @@
     (cond ((number? expr) 1)
           ((string? expr) 1)
           ((symbol? expr) (synth-symbol gamma expr))
+
+	  ; Putting `if` into the type checker as a "good enough for now"
+	  ; approach to conditionals.
+	  ;
+	  ; We're not really totally sure whether `if` or `cond` is the
+	  ; more appropriate primitive to start with. Also, recognizing
+	  ; that extending the type system of each special form likely
+	  ; won't scale, we should revisit this when we've gotten a better
+	  ; handle on macros or more special-form patterns have been
+	  ; introduced.
 	  ([and (pair? expr) (eq? 4 (length expr)) (eq? 'if (car expr))]
 	     (synth-if gamma (cadr expr) (caddr expr) (cadddr expr)))
+
           ((pair? expr) (synth-pair gamma (car expr) (cdr expr))))))
 
 (define repl-with
