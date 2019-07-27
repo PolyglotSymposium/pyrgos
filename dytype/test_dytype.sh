0b945792d68679c4141d0b56f23574e60701dce6
Michael Gilliland <mjg.py3@gmail.com>
2019-07-26 22:27:59 -0400

    Support `λ` as well as `lambda`.
    Not super important but seems like something that's pretty well
    supported (racket & guile but not chicken).

--- dytype/test_dytype.sh
+++ dytype/test_dytype.sh
@@ -43,6 +43,7 @@ assert_unspecified "(42 42)"
 assert_n 1 42
 assert_n 1 '"foo"'
 assert_n 2 '(lambda (x) x)'
+assert_n 2 '(λ (x) x)'
 assert_n 7 '(lambda (a b c d e f) f)'
 
 assert_n 1 '(+ 1 2 3 4 5 6)'
