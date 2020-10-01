24457b6c43bd6056ace9de8d021f7880dc77f274
Keith Pinson <keith.pinson@banno.com>
2020-10-01 17:11:22 -0400

    feat(ennalleen): give myself a shim for testing expr parsing

--- ennalleen/app/Main.hs
+++ ennalleen/app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Ennalleen.Parser
+import System.IO (hFlush, stdout)
+import Text.Megaparsec (parse, errorBundlePretty)
+
+parseAndPrint :: String -> String
+parseAndPrint = either errorBundlePretty show . parse expr "<input>"
+
+loop :: IO ()
+loop = do
+  putStr "> "
+  hFlush stdout
+  text <- getLine
+  if text == ":q"
+  then putStrLn "Goodbye!"
+  else do
+    putStrLn $ parseAndPrint text
+    loop
 
 main :: IO ()
-main = someFunc
+main = loop
--- ennalleen/src/Ennalleen/Syntax.hs
+++ ennalleen/src/Ennalleen/Syntax.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE GADTs #-}
 module Ennalleen.Syntax where
 
-newtype Name = Name String -- TODO this is always the annoying part
+newtype Name =
+  Name String -- TODO this is always the annoying part
+  deriving Show
 
 data Ty where
   TInt :: Ty
   TBool :: Ty
   TFunc :: Ty -> Ty -> Ty
+  deriving Show
 
 data Expr where
   EVar :: Name -> Expr
@@ -21,3 +24,4 @@ data Expr where
   ELambda :: Name -> Expr -> Expr
   EApply :: Expr -> Expr -> Expr
   ELet :: Name -> Expr -> Expr -> Expr
+  deriving Show
