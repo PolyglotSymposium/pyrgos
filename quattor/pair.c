e67f46e70222a27a8fc7726cdebdc1c7ab8b65d7
Keith Pinson <keith.pinson@banno.com>
2021-06-08 12:23:27 -0400

    something I found lying around on my machine

--- quattor/pair.c
+++ quattor/pair.c
@@ -8,21 +8,28 @@ struct Pair {
   void* second;
 };
 
+static Pair* tag_pair_ptr(Pair* pair, bool x_is_ptr, bool y_is_ptr) {
+  return pair; // TODO untag
+}
+
+static Pair* copy_pair_tag(Pair* dst, Pair* src) {
+  return pair; // TODO untag
+}
+
 Pair* make_pair(bool x_is_ptr, void* x, bool y_is_ptr, void* y) {
   Pair* pair = (Pair*)malloc(sizeof(Pair));
   pair->first = x;
   pair->second = y;
-  // TODO tag pair with x_is_ptr and y_is_ptr
-  return pair; // TODO
+  return tag_pair_ptr(pair, x_is_ptr, y_is_ptr);
 }
 
-void* pair_ptr(Pair* pair) {
+Pair* pair_ptr(Pair* pair) {
   return pair; // TODO untag
 }
 
 Pair* copy_pair(Pair* pair) {
-  // TODO untag pair
-  // TODO memcpy pair
-  // TODO tag new pair
-  return pair; // TODO new pair
+  Pair* src = pair_ptr(pair);
+  Pair* dst = (Pair*)malloc(sizeof(Pair));
+  memcpy(dst, src, sizeof(Pair));
+  return tag_pair_ptr(dst, pair);
 }
