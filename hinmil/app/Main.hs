4877189616c61dae2d28d0584dd69753486680bc
Keith Pinson <keith.pinson@banno.com>
2021-11-29 15:19:51 -0500

    yet wrestling with unification

--- hinmil/app/Main.hs
+++ hinmil/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Lib
+import Data.List.NonEmpty (NonEmpty(..))
+
+s1 :: Substitutions
+s1 = sub1 $ Subst (TyVar "a") "x"
+
+s2 :: Substitutions
+s2 = sub1 $ Subst (TyApp "f" (TyVar "b" :| [TyVar "x"])) "y"
+
+s3 :: Substitutions
+s3 = s2 <> s1
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ printSubsts s3
--- hinmil/src/Lib.hs
+++ hinmil/src/Lib.hs
@@ -1,14 +1,28 @@
 module Lib where
 
-import Data.List.NonEmpty (NonEmpty(..))
+import Data.List.NonEmpty (NonEmpty(..), toList)
+import Data.List (intersperse)
+import Control.Monad (join)
 
 type Name = String
 
-data Substitution = Subst Term Name
+data Term = TyVar Name | TyApp Name (NonEmpty Term)
+
+printTerm :: Term -> String
+printTerm (TyVar name) = name
+printTerm (TyApp name args) = name ++ "(" ++ join (intersperse "," $ printTerm <$> toList args) ++ ")"
+
+data Substitution =
+  -- | Substitute in the term when the name is matched.
+  Subst Term Name
+
+printSubst :: Substitution -> String
+printSubst (Subst term name) = printTerm term ++ "/" ++ name
 
 newtype Substitutions = Substs [Substitution]
 
-data Term = TyVar Name | TyApp Name (NonEmpty Term)
+printSubsts :: Substitutions -> String
+printSubsts (Substs xs) = "[" ++ join (intersperse ", " $ printSubst <$> xs) ++ "]"
 
 sub1 :: Substitution -> Substitutions
 sub1 s = Substs [s]
@@ -16,18 +30,32 @@ sub1 s = Substs [s]
 -- | Substitutions closer to the head get precedence over ones closer to the
 -- | tail.
 subs :: Substitutions -> Term -> Term
+-- If we have no substitutions, return the same term.
 subs (Substs []) term = term
+-- If we have hit a leaf of our type AST, to wit, a type variable, then iterate
+-- through the substitutions, looking for a match on its name. If found,
+-- substitute in the corresponding term, and short-circuit.
 subs (Substs (Subst t1 v1 : ss)) term@(TyVar name) =
   if name == v1 then t1 else subs (Substs ss) term
+-- If we have hit a type application, apply all the substitutions to all the
+-- arguments of the type operator.
 subs s (TyApp name args) = TyApp name $ fmap (subs s) args
 
 instance Semigroup Substitution where
-  s' <> (Subst t2 v2) = Subst (subs (sub1 s') t2) v2
+  -- Substitutions on the right are applied to the terms in the substitutions on
+  -- the left.
+  (Subst t2 v2) <> s1 = Subst (subs (sub1 s1) t2) v2
 
 instance Semigroup Substitutions where
-  -- | Substitutions from the left need to be applied to substitutions from the
-  -- | right before being added as the highest precedence substitution on the right.
-  Substs s1 <> Substs s2 = Substs $ foldr (\s ss -> s : fmap (s <>) ss) s2 s1
+  -- Substitutions from the right need to be applied to substitutions from the
+  -- left before being concatenated with the them.
+  -- Substitutions from the right become leftmost (highest-priority).
+  Substs seed <> Substs foldee = Substs $ foldr oneSubst seed foldee where
+    -- We take the next highest-precedence substitution from the right, make it
+    -- our new highest-precedence substitution, and apply it to all of the
+    -- existing lower-precedence substitutions.
+    oneSubst :: Substitution -> [Substitution] -> [Substitution]
+    oneSubst s ss = s : fmap (<> s) ss
 
 data UnificationFailure = ArityMismatch | CircularOccurence | TypeFunctionMismatch
 
