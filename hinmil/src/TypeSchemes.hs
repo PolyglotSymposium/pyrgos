5bfd59bdf239ce393f03f1224a635133fd04b6eb
Keith Pinson <keith.pinson@banno.com>
2022-02-14 12:50:39 -0500

    refactor(hinmil): clean up type schemes

--- hinmil/src/TypeSchemes.hs
+++ hinmil/src/TypeSchemes.hs
@@ -1,31 +1,25 @@
 module TypeSchemes where
 
 import TypeAST
-import Data.List.NonEmpty (NonEmpty(..))
+import Data.Foldable (foldl')
 
 data TypeScheme =
   Forall String TypeScheme |
   Type Term
 
--- TODO NonEmpty probably becoming a bad idea
-freeAndBoundInType :: [Name] -> [Name] -> Term -> ([Name], [Name])
-freeAndBoundInType free bound (TyVar v) =
-  if elem v bound
-  then (free, bound)
-  else (v : free, bound)
-freeAndBoundInType free bound (TyApp _ args) =
-  let iter r (t :| []) = fst $ freeAndBoundInType r bound t
-      iter r (t :| t' : ts) =
-        let (free', _) = freeAndBoundInType r bound t
-        in iter free' (t' :| ts)
-      fvs = iter free args
-  in (fvs, bound)
+freeInType :: [Name] -> [Name] -> Term -> [Name]
+freeInType free bound (TyVar var) =
+  if elem var bound
+  then free
+  else var : free
+freeInType free bound (TyApp _ args) =
+  foldl' (\r t -> freeInType r bound t) free args
 
 freeAndBoundInScheme :: [Name] -> [String] -> TypeScheme -> ([Name], [Name])
-freeAndBoundInScheme f b (Forall v s) = freeAndBoundInScheme f (v : b) s
-freeAndBoundInScheme f b (Type t) = freeAndBoundInType f b t
+freeAndBoundInScheme free bound (Forall var scheme) =
+  freeAndBoundInScheme free (var : bound) scheme
+freeAndBoundInScheme free bound (Type typ) =
+  (freeInType free bound typ, bound)
 
 tyVars :: Term -> [Name]
-tyVars t =
-  let (free, bound) = freeAndBoundInType [] [] t
-  in free ++ bound
+tyVars = freeInType [] []
